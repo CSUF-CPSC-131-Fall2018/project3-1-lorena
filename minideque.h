7313f318a46fbc0663ab144e76400a079f6ade21
lorena <36055216+LorenaMacias@users.noreply.github.com>
2018-10-13 18:56:32 -0700

    Update minideque.h

--- minideque.h
+++ minideque.h
@@ -29,6 +29,8 @@ public:
   size_t fronthalfsize() const;    // TODO
   size_t backhalfsize() const;     // TODO
   bool empty()  const;              // TODO
+  
+  int split; //i'm using this to know # of elements in each vector after split
 
   // balance queue across both vectors if pop_front/back is requested on an empty vector
   // e.g., minideque has this:                  | ABCDEFG
@@ -78,5 +80,104 @@ public:
     }
   }
 };
+
+size_t minideque::size() const{
+  return fronthalf_.size() + backhalf_.size();
+}
+
+size_t minideque::fronthalfsize() const {
+		return fronthalf_.size();
+}
+
+size_t minideque::backhalfsize() const {
+		return backhalf_.size();
+	}
+
+bool minideque::empty()  const {
+		if (fronthalf_.empty() == true && backhalf_.empty() == true)
+			return true;
+		else {
+			return false;
+		}
+}
+
+void minideque::pop_front() {
+
+		if (fronthalfsize() == 0) {
+			backhalf_.erase(vec.begin()); //erase the front of backhalf 
+			split = backhalfsize() / 2;
+			for (int i = 0; i < split; i++) {
+				fronthalf_[i].push_back( backhalf_.begin() ); //assign the back elements of backhalf to fronthalf
+				backhalf_.erase(backhalf_.begin()); //delete the elements of backhalf that were copied
+			}
+		}
+		else {
+			fronthalf_.pop_back();
+		}
+}
+
+void minideque::pop_back() {
+
+		if (backhalfsize() == 0) {
+			fronthalf_.pop_back();
+			split = fronthalfsize() / 2;
+			for (int i = 0; i < split; i++)
+			{
+				backhalf_[i].push_back(fronthalf_.back());
+				fronthalf_.erase(fronthalf_.back());
+			}
+		}
+		else {
+			backhalf_.pop_back();
+		}
+}
+
+void minideque::push_front(const T& value) {
+		fronthalf_.push_back(value); //the back of the vector is the 'front'
+	}
+
+void minideque::push_back(const T& value) {
+		backhalf_.push_back(value)
+	}
+
+const T& minideque::front() const {
+		if (fronthalfsize() == 0) {
+			return backhalf_.front();
+		}
+		else {
+			return fronthalf_.back();
+		}
+}
+
+const T& minideque::back() const {
+		if (backhalfsize() == 0) {
+			return fronthalf_.front();
+		}
+
+		else {
+		return backhalf_.back();
+    }
+}
+
+T& minideque::front() {
+		if (fronthalfsize() == 0) {
+			return backhalf_.front();
+		}
+		else {
+			return fronthalf_.back();
+		}
+	}
+
+T& minideque::back() {
+		if (backhalfsize() == 0) {
+			return fronthalf_.front();
+		}
+
+		else {
+			return backhalf_.back();
+		}
+}
+
+
 
 #endif /* minideque_h */
